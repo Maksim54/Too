ff81b76b918d079e494fc1990e775c3b41463861
Maksim54 <77334211+Maksim54@users.noreply.github.com>
2021-10-27 11:44:59 +0300

    Написаны комментарии.

--- iseseisev_too/Program.cs
+++ iseseisev_too/Program.cs
@@ -10,25 +10,30 @@ namespace iseseisev_too
     {
         static int saali_suurus()
         {
-            Console.WriteLine("Vali saali suurus:1,2,3");
+            Console.WriteLine("Choose the size of the hall: 1,2,3");//выбираем тип размера для зала 1,2 или 3
             int suurus = int.Parse(Console.ReadLine());
-            return suurus;
+            return suurus;//возвращаем значение suurus
         }
-        static int[,] saal = new int[,] { };
-        static int kohad, read;
-        static void Saali_taitmine(int suurus)
+        static int[,] saal = new int[,] { };//создаётся пустой двумерный массив
+        static int[] ost = new int[] { };//хранит переменную ost и saal
+        static int kohad, read,mitu,mitu_veel;
+        static void Saali_taitmine(int suurus)//используется в качестве возвращаемого типа метода,чтобы указать,что метод не возвращает значение.
         {
-            Random rnd = new Random();
-            if (suurus == 1)
-            {kohad = 20;read = 10;}
-            else if (suurus==2)
-            {kohad = 20;read = 20;}
+            Random rnd = new Random();//генерируем значения kohad и read через random
+            if (suurus == 1)//делается проверка
+            {
+                kohad = 20;read = 10;
+            }
+            else if (suurus==2)//делается проверка
+            {
+                kohad = 20;read = 20;
+            }
             else
             {
                 kohad = 30;
                 read = 20;
             }
-            saal = new int[read, kohad];
+            saal = new int[read, kohad];//
             for (int rida = 0; rida < read; rida++)
             {
                 for (int koht = 0; koht < kohad; koht++)
@@ -38,62 +43,130 @@ namespace iseseisev_too
             }
         }
 
-        static void Saal_ekraanile()
+        static void Saal_ekraanile()//
         {
-            Console.Write("     ");
+            Console.Write("     ");//
             for (int koht = 0; koht < kohad; koht++)
             {
                 if (koht.ToString().Length == 2)
-                {
-                    Console.Write(" {0}", koht + 1);
-                }
+                { Console.Write(" {0}", koht + 1); }
                 else
-                {
-                    Console.Write(" {0}", koht + 1);
-                }
+                { Console.Write(" {0}", koht + 1); }
             }
-                Console.WriteLine();
-                for (int rida = 0; rida < read; rida++)
-                {
-                    Console.Write("rida " + (rida + 1).ToString() + ":");
-                    for (int koht = 0; koht < kohad; koht++)
-                    {
 
-                    }
+            Console.WriteLine();//
+            for (int rida = 0; rida < read; rida++)
+            {
+                Console.Write("Row " + (rida + 1).ToString() + ":");
+                for (int koht = 0; koht < kohad; koht++)
+                {
+
+                    Console.Write(saal[rida, koht] + "  ");//к каждому номеру добавляется +1 тем самым увеличивает число пока не кончатся места в зале
                 }
+                Console.WriteLine();//пустая строка после окончания мест
+            }
         }
         static int pileti_koht;
 
         static bool Muuk()
-        {            
-            Console.WriteLine("rida-->");
+        {
+            Console.WriteLine("Row:");//выбираем нужный нам ряд
             int pileti_rida = int.Parse(Console.ReadLine());
-            Console.WriteLine("Mitu piletid-->");
-            int mitu = int.Parse(Console.ReadLine());
-            int p = kohad / 2;
-                if (saal[pileti_rida - 1,p] == 0)
+            Console.WriteLine("How many tickets you want?:");//записываем сколько билетов мы хотим купить
+            mitu = int.Parse(Console.ReadLine());//
+            ost = new int[mitu];//
+            int p = (kohad - mitu) / 2;//
+            bool t = false;//объявляем логическую переменную t
+            int k = 0;//значение k приравниваем нулю
+            do
+            {
+                if (saal[pileti_rida, p] == 0)//Если в зале место обозначено нулем,то оно является свободным
                 {
-                    Console.WriteLine("Koht {0} on vaba", p);
-                    return true;
+                    ost[k] = p;
+                    Console.WriteLine("place {0} is free", p);//отображает свободные места
+                    t = true;
                 }
                 else
                 {
-                    Console.WriteLine("Koht {0} on kinni", p);
-                    return false;
+                    Console.WriteLine("place {0} booked", p);//отображает места,которые забронированы
+                    t = false;
+                    ost = new int[mitu];
+                    k = 0;//
+                    p = (kohad - mitu) / 2;//
+                    break;//делаем сброс переменной
                 }
+                p = p + 1;//+ к месту
+                k++;
+            } while (mitu != k);//в операторе while 
+            if (t==true)
+            {
+                Console.WriteLine("your place is:");//отображает места,которые выбрал пользователь
+                foreach (var koh in ost)//цикл начинается, первый элемент массива выбирается и присваивается переменной цикла
+                {
+                    Console.WriteLine("{0}\n", koh);
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no vacancies in this row. Do you want to search in the second row?");//если место занято,то мы должны выбрать любое другое,которое свободно
+            }
+            return t;//возвращаем значение t
         }
 
-        static void Main(string[] args)
+        static bool muuk_ise()
+        {
+            Console.WriteLine("Row:");
+            int pileti_rida = int.Parse(Console.ReadLine());
+            Console.WriteLine("Row:");
+            int pileti_koht = int.Parse(Console.ReadLine());
+            if (saal[pileti_rida,pileti_koht] == 0)
+            {
+                saal[pileti_rida, pileti_koht] = 1;
+            }
+        }
+
+        static void Main(string[] args)//
         {
             int suurus = saali_suurus();
-            Random rnd = new Random();
-            Saali_taitmine(suurus);
+            Saali_taitmine(suurus);//
             while (true)
             {
                 Saal_ekraanile();
-                bool ost = Muuk();
-                Console.WriteLine(ost);
+                int valik = int.Parse(Console.ReadLine());
+                Console.WriteLine("1-own choice, 2-machine choice");
+                if (valik == 1)
+                {
+                    int koh = 0;
+                    Console.WriteLine("How many tickets want to buy?");
+                    int kogus = int.Parse(Console.ReadLine());
+                    bool muuk = false;
+                    while (muuk!=true)
+                    {
+                        for (int i = 0; i < (kohad - 1) * (read - 1); i++)//kui valime 0-d
+                        {
+                            muuk = muuk_ise();
+                            if (muuk) { koh++; }
+                            if (koh==kogus) { break; }
+                            {
+                                koh++;
+                            }
+                            if (koh == kogus)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    bool muuk = false;
+                    while (Muuk()!=true)
+                    {
+                        muuk=Muuk();
+                    }
+                }
             }
+            Console.ReadKey();
             //Console.ReadLine();
         }
     }
